0278d28b16010246682ee4912931ace8661025c9
ropkyns <ropkyns@gmail.com>
2023-11-27 00:03:11 +0100

    push mandatory n makefile

--- ft_isascii.c
+++ ft_isascii.c
@@ -3,32 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ropkyns <ropkyns@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:46:36 by paulmart          #+#    #+#             */
-/*   Updated: 2023/11/13 14:52:42 by paulmart         ###   ########.fr       */
+/*   Updated: 2023/11/27 00:02:59 by ropkyns          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
-#include <stdio.h>
 #include "libft.h"
 
 int ft_isascii(int c);
 
 int ft_isascii(int c)
 {
-    if ((c >= 0) && (c <= 127))
-        return (1);
-    else
-        return (0);
+	if((c >= 0) && (c <= 127))
+		return (1);
+	else
+		return (0);
 }
 
 /* int main(void)
 {
-    int c = 1288;
-    
-    printf("%d", ft_isascii(c));
-    printf("%d", isascii(c));
-    return 0;
+	int c = 1288;
+	
+	printf("%d", ft_isascii(c));
+	printf("%d", isascii(c));
+	return 0;
 } */
