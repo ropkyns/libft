5e551a8ccfa5542ef531d3bfabd7a1b08e272400
Paul Martins Pereira <paulmart@made-f0Dr4s2.clusters.42paris.fr>
2023-12-07 19:10:25 +0100

    pre bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 14:14:21 by paulmart          #+#    #+#             */
-/*   Updated: 2023/12/07 17:56:11 by paulmart         ###   ########.fr       */
+/*   Updated: 2023/12/07 19:02:45 by paulmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,21 @@
 # include <stdlib.h>
 
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *str, size_t n);
